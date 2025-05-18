286145617e79b7cc8ce6b994fd9cd6b1cf57d0df
Ranz <basti020613@gmail.com>
2025-05-18 17:48:29 +0400

    Web Dev assessment-2

---
+++ battlespells.js
@@ -0,0 +1,148 @@
+
+  const btn1 = document.getElementById('button1');
+const desc1 = document.getElementById('desc1');
+btn1.addEventListener('click', function() {
+  if (desc1.style.display === 'none') {
+    desc1.style.display = 'block';
+    btn1.textContent = 'Execute';
+  } else {
+    desc1.style.display = 'none';
+    btn1.textContent = 'Execute';
+  }
+});
+
+ const btn2 = document.getElementById('button2');
+const desc2 = document.getElementById('desc2');
+btn2.addEventListener('click', function() {
+  if (desc2.style.display === 'none') {
+    desc2.style.display = 'block';
+    btn2.textContent = 'Retribution';
+  } else {
+    desc2.style.display = 'none';
+    btn2.textContent = 'Retribution';
+  }
+});
+
+
+ const btn3 = document.getElementById('button3');
+const desc3 = document.getElementById('desc3');
+btn3.addEventListener('click', function() {
+  if (desc3.style.display === 'none') {
+    desc3.style.display = 'block';
+    btn3.textContent = 'Inspire';
+  } else {
+    desc3.style.display = 'none';
+    btn3.textContent = 'Inspire';
+  }
+});
+
+const btn4 = document.getElementById('button4');
+const desc4 = document.getElementById('desc4');
+btn4.addEventListener('click', function() {
+  if (desc4.style.display === 'none') {
+    desc4.style.display = 'block';
+    btn4.textContent = 'Sprint';
+  } else {
+    desc4.style.display = 'none';
+    btn4.textContent = 'Sprint';
+  }
+});
+
+const btn5 = document.getElementById('button5');
+const desc5 = document.getElementById('desc5');
+btn5.addEventListener('click', function() {
+  if (desc5.style.display === 'none') {
+    desc5.style.display = 'block';
+    btn5.textContent = 'Revitalize';
+  } else {
+    desc5.style.display = 'none';
+    btn5.textContent = 'Revitalize';
+  }
+});
+
+const btn6 = document.getElementById('button6');
+const desc6 = document.getElementById('desc6');
+btn6.addEventListener('click', function() {
+  if (desc6.style.display === 'none') {
+    desc6.style.display = 'block';
+    btn6.textContent = 'Aegis';
+  } else {
+    desc6.style.display = 'none';
+    btn6.textContent = 'Aegis';
+  }
+});
+
+const btn7 = document.getElementById('button7');
+const desc7 = document.getElementById('desc7');
+btn7.addEventListener('click', function() {
+  if (desc7.style.display === 'none') {
+    desc7.style.display = 'block';
+    btn7.textContent = 'Petrify';
+  } else {
+    desc7.style.display = 'none';
+    btn7.textContent = 'Petrify';
+  }
+});
+
+const btn8 = document.getElementById('button8');
+const desc8 = document.getElementById('desc8');
+btn8.addEventListener('click', function() {
+  if (desc8.style.display === 'none') {
+    desc8.style.display = 'block';
+    btn8.textContent = 'Flameshot';
+  } else {
+    desc8.style.display = 'none';
+    btn8.textContent = 'Flameshot';
+  }
+});
+
+const btn9 = document.getElementById('button9');
+const desc9 = document.getElementById('desc9');
+btn9.addEventListener('click', function() {
+  if (desc9.style.display === 'none') {
+    desc9.style.display = 'block';
+    btn9.textContent = 'Flicker';
+  } else {
+    desc9.style.display = 'none';
+    btn9.textContent = 'Flicker';
+  }
+});
+
+
+const btn11 = document.getElementById('button11');
+const desc11 = document.getElementById('desc11');
+btn11.addEventListener('click', function() {
+  if (desc11.style.display === 'none') {
+    desc11.style.display = 'block';
+    btn11.textContent = 'Arrival';
+  } else {
+    desc11.style.display = 'none';
+    btn11.textContent = 'Arrival';
+  }
+});
+
+
+const btn12 = document.getElementById('button12');
+const desc12 = document.getElementById('desc12');
+btn12.addEventListener('click', function() {
+  if (desc12.style.display === 'none') {
+    desc12.style.display = 'block';
+    btn12.textContent = 'Vengeance';
+  } else {
+    desc12.style.display = 'none';
+    btn12.textContent = 'Vengeance';
+  }
+});
+
+
+const btn13 = document.getElementById('button13');
+const desc13 = document.getElementById('desc13');
+btn13.addEventListener('click', function() {
+  if (desc13.style.display === 'none') {
+    desc13.style.display = 'block';
+    btn13.textContent = 'Purify';
+  } else {
+    desc13.style.display = 'none';
+    btn13.textContent = 'Purify';
+  }
+});
